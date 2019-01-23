e048ee76d56dcc038c585acddb964e33a95f1e11
gitsuh <21348752+gitsuh@users.noreply.github.com>
2019-01-23 16:55:04 -0500

    added

--- toolsfunctions.ps1
+++ toolsfunctions.ps1
@@ -13,4 +13,21 @@ function moveit {
 	foreach ($file in $files){
 		Start-BitsTransfer -Source $file.fullname -Destination \\$targetserver\$targetlocation$file.name -priority low -asynchronous
 	}
+	
+	$jobs = get-bitstransfer
+
+	$alljobscompleted = $false
+
+	while (!$alljobscompleted){
+		$jobs = get-bitstransfer
+		write-host "Waiting for all jobs to queue and finish transferring."
+		if($jobs.jobstate -contains "Queued" -or $jobs.jobstate -contains "Transferring"){
+			$alljobscompleted = $false
+		}else{
+			$alljobscompleted = $true
+		}
+	}
+	$jobs | complete-bitstransfer
 }
+
+
